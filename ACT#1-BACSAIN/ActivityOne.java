a42dc465c3734937f7e4e2819b0cca90ebc39a5c
trtlbby <earllawrence.bacsain@unc.edu.ph>
2026-02-04 12:38:28 +0800

    Added the User Entry Module. Not yet checked. Gonna take a nap

--- ACT#1-BACSAIN/ActivityOne.java
+++ ACT#1-BACSAIN/ActivityOne.java
@@ -74,9 +74,60 @@ class ActivityOne {
         });
     }
 
-    
+    public void UserEntryModule(){
+        frame = new JFrame("User Entry Module");
+        fnameField = new JTextField();
+        lnameField = new JTextField();
+        emailField = new JTextField();
+        passwordField = new JPasswordField();
+
+        JPanel panel = new JPanel(new GridLayout(5, 2, 10, 10));
+        panel.add(new JLabel("Enter First Name: "));
+        panel.add(fnameField);
+        panel.add(new JLabel("Enter Last Name"));
+        panel.add(lnameField);
+        panel.add(new JLabel("Enter Email: "));
+        panel.add(emailField);
+        panel.add(new JLabel("Enter Password: "));
+        panel.add(passwordField);
+        panel.add(new JLabel());
+        panel.add(submitButton);
+
+        frame.setLocationRelativeTo(null);
+        frame.setTitle("User Entry Module");
+        frame.add(panel);
+        frame.pack();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+        submitButton.addActionListener(e -> {
+            String first_name = fnameField.getText();
+            String last_name = lnameField.getText();
+            String email = emailField.getText();
+            char[] password = passwordField.getPassword();
+        
+            String sql = "INSERT INTO users (first_name, last_name, email, password) values (?, ?, ?,?)";
+            try {
+                PreparedStatement stmt = db.conn.prepareStatement(sql);
+                stmt.setString(1, first_name);
+                stmt.setString(2, last_name);
+                stmt.setString(3, email);
+                stmt.setString(4, Utils.hashPassword(new String(password)));
+                stmt.executeUpdate();
+                JOptionPane.showMessageDialog(frame, "Employee added successfully!");
+
+                stmt.close();
+                db.conn.close();
+            } catch (java.sql.SQLException ex) {
+                ex.printStackTrace();
+                JOptionPane.showMessageDialog(frame, "Database error: " + ex.getMessage());
+                System.out.println("Database error: " + ex.getMessage());
+            }
+        });
+    }
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(()-> new ActivityOne().EmployeeEntryModule());
+        SwingUtilities.invokeLater(()-> new ActivityOne().UserEntryModule());
     }
 }
